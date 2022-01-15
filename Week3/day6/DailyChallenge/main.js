7062b11886698dd0dacefa94e33232a05fbb75e1
Yotam-D <d.yotam@gmail.com>
2022-01-15 22:15:35 +0200

    + Daily challenge

---
+++ Week3/day6/DailyChallenge/main.js
@@ -0,0 +1,26 @@
+//Daily Challange
+for (var i = 0; i < 5; i++) {
+    console.log('\*'.repeat(i));
+}
+
+
+
+// first way
+let array = [
+  [1,2],
+  [3,4]
+];
+for (var i = 0; i < 2; i++) {
+  for (var j = 0; j < 2; j++) {
+    console.log('\*'.repeat(array[i][j]));
+  }
+}
+
+// second way
+strToLog = '';
+for (var i = 0; i < 2; i++) {
+  for (var j = 0; j < 2; j++) {
+    strToLog = '\*'.repeat(i*2) +  '\*'.repeat(j+1)
+    console.log(strToLog);
+  }
+}
