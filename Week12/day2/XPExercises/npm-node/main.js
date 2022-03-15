153d2a28df6f5f048f32704583bcf3a43de4af4a
Yotam-D <d.yotam@gmail.com>
2022-03-15 16:12:31 +0200

    Week12 day2 XP

---
+++ Week12/day2/XPExercises/npm-node/main.js
@@ -0,0 +1,12 @@
+// EX1
+// let a = 5;
+// let b = 10;
+// console.log(`hello you are ${a+b}`);
+
+// EX3
+let a = 5;
+let b = 10;
+let x;
+let c = 6;
+x = a + b;
+console.log(`sum of a+b = ${x+c}`);
